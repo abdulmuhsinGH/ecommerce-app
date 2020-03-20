a55c6a0c69c192ebd23b3f7699b12af775f0d1e5
Abdul Muhsin Iddris <abdul.iddris13@gmail.com>
2020-03-20 00:30:08 +0000

    client store with postgres

--- auth-server/pkg/auth/client_store.go
+++ auth-server/pkg/auth/client_store.go
@@ -1,8 +1,61 @@
 package auth
 
-/* 
+import (
+	"fmt"
+
+	"github.com/go-pg/pg/v9"
+	"gopkg.in/oauth2.v3"
+	"gopkg.in/oauth2.v3/models"
+)
+
+/*
+ClientStore is an interface for this
+*/
+type ClientStore struct {
+	db *pg.DB
+}
+
+/*
+OauthClient is model for the oauth_clients table
+*/
+type OauthClient struct {
+	ID     string                 `db:"id"`
+	Secret string                 `db:"secret"`
+	Domain string                 `db:"domain"`
+	Data   map[string]interface{} `db:"data"`
+}
+
+/*
+NewClientStore sets up the client store object
+*/
+func NewClientStore(db *pg.DB) *ClientStore {
+	return &ClientStore{db}
+}
+
+/*
 GetByID return client details using id
 */
-func GetByID(ID string)  {
-	
+func (c *ClientStore) GetByID(ID string) (oauth2.ClientInfo, error) {
+	oauthClient := OauthClient{ID: ID}
+	err := c.db.Select(&oauthClient)
+	if err != nil {
+		fmt.Println("oc", err.Error())
+		return nil, err
+	}
+	fmt.Println(oauthClient)
+	clientInfo := c.toClientInfo(oauthClient)
+	if err != nil {
+		fmt.Println("ci", err.Error())
+		return nil, err
+	}
+	return clientInfo, nil
+}
+
+func (c *ClientStore) toClientInfo(data OauthClient) oauth2.ClientInfo {
+	var cm models.Client
+	cm.ID = data.ID
+	cm.Secret = data.Secret
+	cm.Domain = data.Domain
+	//err := json.Unmarshal(data, &cm)
+	return &cm
 }
--- auth-server/pkg/auth/server.go
+++ auth-server/pkg/auth/server.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/go-pg/pg/v9"
@@ -16,7 +15,6 @@ import (
 	"gopkg.in/oauth2.v3/errors"
 	"gopkg.in/oauth2.v3/generates"
 	"gopkg.in/oauth2.v3/manage"
-	"gopkg.in/oauth2.v3/models"
 	"gopkg.in/oauth2.v3/server"
 	"gopkg.in/oauth2.v3/store"
 )
@@ -46,33 +44,19 @@ func Server(db *pg.DB, logging logging.Logging) {
 	// generate jwt access token
 	manager.MapAccessGenerate(generates.NewJWTAccessGenerate([]byte("00000000"), jwt.SigningMethodHS512))
 	fmt.Println("Hello auth server")
-	testClientStore = store.NewClientStore()
-	testClientStore.Set(os.Getenv("test_client_id"), &models.Client{
-		ID:     os.Getenv("test_client_id"),
-		Secret: os.Getenv("test_client_secret"),
-		Domain: os.Getenv("test_client_domain"),
-	})
-	manager.MapClientStorage(testClientStore)
 
-	adminClientStore = store.NewClientStore()
-	adminClientStore.Set(os.Getenv("admin_client_id"), &models.Client{
-		ID:     os.Getenv("admin_client_id"),
-		Secret: os.Getenv("admin_client_secret"),
-		Domain: os.Getenv("admin_client_domain"),
-	})
+	clientStore := NewClientStore(db)
 
-	manager.MapClientStorage(adminClientStore)
+	manager.MapClientStorage(clientStore)
 
 	srv := server.NewServer(server.NewConfig(), manager)
-	/* srv.SetPasswordAuthorizationHandler(func(username, password string) (userID string, err error) {
+	srv.SetPasswordAuthorizationHandler(func(username, password string) (userID string, err error) {
 		user, err := authService.Login(username, password)
 		if err != nil {
 			return "", err
 		}
 		return user.ID.String(), nil
-	}) */
-
-	srv.SetClientInfoHandler(setClientInfoHandler)
+	})
 	srv.SetUserAuthorizationHandler(userAuthorizeHandler)
 
 	srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
@@ -89,42 +73,6 @@ func Server(db *pg.DB, logging logging.Logging) {
 	logging.Printlog("AuthServer", "Server is running at 9096 port.")
 
 	log.Fatal(http.ListenAndServe(":9096", router))
-}
-
-func setClientInfoHandler(r *http.Request) (clientID, clientSecret string, err error) {
-	fmt.Println("Hello set client info")
-
-	clientInfo := r.URL.Query()
-
-	if clientInfo.Get("client_id") == os.Getenv("admin_client_id") {
-		_, err := manager.GetClient(os.Getenv("admin_client_id"))
-		if err != nil {
-			//adminClientStore := store.NewClientStore()
-			adminClientStore.Set(os.Getenv("admin_client_id"), &models.Client{
-				ID:     os.Getenv("admin_client_id"),
-				Secret: os.Getenv("admin_client_secret"),
-				Domain: os.Getenv("admin_client_domain"),
-			})
-			manager.MapClientStorage(adminClientStore)
-		}
-	} else if clientInfo.Get("client_id") == os.Getenv("test_client_id") {
-
-		_, err := manager.GetClient(os.Getenv("test_client_id"))
-		if err != nil {
-			fmt.Println("authorizrh", err.Error())
-			//testClientStore := store.NewClientStore()
-			testClientStore.Set(os.Getenv("test_client_id"), &models.Client{
-				ID:     os.Getenv("test_client_id"),
-				Secret: os.Getenv("test_client_secret"),
-				Domain: os.Getenv("test_client_domain"),
-			})
-			manager.MapClientStorage(testClientStore)
-		}
-	}
-	clientID = clientInfo.Get("client_id")
-	clientSecret = clientInfo.Get("clinet_secret")
-
-	return
 }
 
 func userAuthorizeHandler(w http.ResponseWriter, r *http.Request) (userID string, err error) {
